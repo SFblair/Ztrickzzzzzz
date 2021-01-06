a8f9faf68c224654167f6d6e2376d9c3a6f19398
SFblair_yt <72683929+SFblair@users.noreply.github.com>
2021-01-06 13:55:05 -0800

    Add files via upload

---
+++ Ztrickzzzzz.py
@@ -0,0 +1,24 @@
+def menu():
+    print("[1] run name")
+    print("[2] name vault")
+    print("[0] exit the program.")
+
+
+menu()
+option = int(input("enter your option")
+
+while option != 0:
+    if option == 1
+    #do run name stuff
+    print("running Ztrickzzzzzz.")
+elif  option == 2:
+    #do name vault stuff
+    print("working name vault.")
+else:
+    print("invalid run."
+
+print("pls type name you want to snipe.")
+    menu()
+option = int(input("enter your option")
+
+print("name snipe failed.    try later.")
---
+++ names.py
@@ -0,0 +1,13 @@
+import os
+
+print("Ztrickzzzzz by SFblair./n(c) 2020 name sniper mc all rights reserved")
+
+while True:
+    cmd = input("os_windows>")
+
+if cmd =="":
+    print("\n")
+
+else:
+
+    os.system.(cmd)
